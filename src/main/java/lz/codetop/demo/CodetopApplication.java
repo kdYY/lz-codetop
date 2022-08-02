ec1114afbabdaeab65cc232ef16cc87d726d6590
yaokaidong <yaokaidong@lizhi.fm>
2022-08-02 14:36:38 +0800

    kd

---
+++ src/main/java/lz/codetop/demo/CodetopApplication.java
@@ -0,0 +1,13 @@
+package lz.codetop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodetopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodetopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lz/codetop/demo/CodetopApplicationTests.java
@@ -0,0 +1,13 @@
+package lz.codetop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodetopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
